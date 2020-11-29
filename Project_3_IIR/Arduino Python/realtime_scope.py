3078b02d9edeba0524aaf940d3c49eaeecc9a80e
allancotto <allancotto@outlook.com>
2020-11-29 17:53:42 +0000

    Added arduino supporting files
    Added python files
    Added StandardFirmata.ino

---
+++ Project_3_IIR/Arduino Python/realtime_scope.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+from pyfirmata2 import Arduino
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# Realtime oscilloscope at a sampling rate of 50Hz
+# It displays analog channel 0.
+# You can plot multiple channels just by instantiating
+# more RealtimePlotWindow instances and registering
+# callbacks from the other channels.
+
+
+PORT = Arduino.AUTODETECT
+# PORT = '/dev/ttyUSB0'
+
+# Creates a scrolling data display
+class RealtimePlotWindow:
+
+    def __init__(self):
+        # create a plot window
+        self.fig, self.ax = plt.subplots()
+        # that's our plotbuffer
+        self.plotbuffer = np.zeros(500)
+        # create an empty line
+        self.line, = self.ax.plot(self.plotbuffer)
+        # axis
+        self.ax.set_ylim(0, 1)
+        # That's our ringbuffer which accumulates the samples
+        # It's emptied every time when the plot window below
+        # does a repaint
+        self.ringbuffer = []
+        # add any initialisation code here (filters etc)
+        # start the animation
+        self.ani = animation.FuncAnimation(self.fig, self.update, interval=100)
+
+    # updates the plot
+    def update(self, data):
+        # add new data to the buffer
+        self.plotbuffer = np.append(self.plotbuffer, self.ringbuffer)
+        # only keep the 500 newest ones and discard the old ones
+        self.plotbuffer = self.plotbuffer[-500:]
+        self.ringbuffer = []
+        # set the new 500 points of channel 9
+        self.line.set_ydata(self.plotbuffer)
+        return self.line,
+
+    # appends data to the ringbuffer
+    def addData(self, v):
+        self.ringbuffer.append(v)
+
+
+# Create an instance of an animated scrolling window
+# To plot more channels just create more instances and add callback handlers below
+realtimePlotWindow = RealtimePlotWindow()
+realtimePlotWindow = RealtimePlotWindow()
+# sampling rate: 100Hz
+samplingRate = 100
+
+# called for every new sample which has arrived from the Arduino
+def callBack(data):
+    # send the sample to the plotwindow
+    # add any filtering here:
+    # data = self.myfilter.dofilter(data)
+    realtimePlotWindow.addData(data)
+
+# Get the Arduino board.
+board = Arduino(PORT)
+
+# Set the sampling rate in the Arduino
+board.samplingOn(1000 / samplingRate)
+
+# Register the callback which adds the data to the animated plot
+board.analog[0].register_callback(callBack)
+# Register the second callback which adds the data to the animated plot - (Allan)
+board.analog[1].register_callback(callBack())
+
+
+# Enable the callback
+board.analog[0].enable_reporting()
+# Enable the second callback (allan)
+board.analog[1].enable_reporting()
+
+# show the plot and start the animation
+plt.show()
+
+# needs to be called to close the serial port
+board.exit()
+
+print("finished")
